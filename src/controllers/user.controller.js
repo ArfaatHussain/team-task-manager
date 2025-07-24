8a4d8bbdcb06e723488e1bce991cc46a4d939b92
ArfaatHussain <arfaathussain745@gmail.com>
2025-07-24 16:24:59 +0500

    Add updateUserProfile controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -126,4 +126,61 @@ const getUserProfile = asyncHandler( async(req,res)=>{
     })
 } )
 
-export { registerUser, loginUser, getUserProfile }
+const updateUserProfile = asyncHandler( async(req,res)=>{
+    const {userId,username, email, currentPassword, newPassword} = req.body
+
+    if(!userId){
+        throw new ApiError(400,"User ID is missing")
+    }
+    if(!username && !email && !currentPassword){
+        throw new ApiError(400,"Please provide atleast one field to update")
+    }
+
+    let fieldsToUpdate = {}
+
+    if(username){
+        fieldsToUpdate.username = username
+    }
+    if(email){
+        fieldsToUpdate.email = email
+    }
+    if(currentPassword){
+        if(!newPassword){
+            throw new ApiError(400,"New password field is missing")
+        }
+        fieldsToUpdate.password = newPassword
+    }
+
+    const user = await User.findOne({where:{
+        id: userId
+    }})
+
+    if(!user){
+        throw new ApiError(404,"User not found with this email or username")
+    }
+
+    const isPasswordCorrect = await user.isPasswordCorrect(currentPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(401,"Current password is invalid")
+    }
+
+    if(fieldsToUpdate.username){
+        user.username = username
+    }
+    if(fieldsToUpdate.email){
+        user.email = email
+    }
+    if(fieldsToUpdate.password){
+        const hashedPassword = await bcrypt.hash(newPassword,10)
+        user.password = hashedPassword
+    }
+
+    await user.save()
+
+    res.status(200).json({
+        message: "success"
+    })
+} )
+
+export { registerUser, loginUser, getUserProfile, updateUserProfile }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, getUserProfile } from "../controllers/user.controller.js";
+import { registerUser, loginUser, getUserProfile, updateUserProfile } from "../controllers/user.controller.js";
 
 const userRouter = Router()
 
@@ -8,5 +8,7 @@ userRouter.route("/register").post(registerUser)
 userRouter.route("/login").post(loginUser)
 
 userRouter.route("/getUserProfile/:userId").get(getUserProfile)
+
+userRouter.route("/updateUserProfile").patch(updateUserProfile)
 
 export {userRouter}
